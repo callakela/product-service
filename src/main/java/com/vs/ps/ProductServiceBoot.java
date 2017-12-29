7f30274b5f2da072268dbea73c9c0c09fce3145e
Amit Kumar Akela <amitk.akela@impetus.co.in>
2017-12-29 14:03:35 +0530

    refactor

--- src/main/java/com/vs/ps/ProductServiceBoot.java
+++ src/main/java/com/vs/ps/ProductServiceBoot.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-//@EnableSwagger2
 @ComponentScan(basePackages = "com.vs.ps")
 @EnableCaching
 
